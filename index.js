280408db249308d61b98b41e44c94de93d686a10
Shakib-Shajid <shakibalshajid1682@gmail.com>
2024-07-27 18:33:19 +0600

    comment added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 
@@ -10,7 +11,6 @@ app.use(express.json());
 
 // ...........................................................................
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pvn5rcy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,6 +26,101 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    // ...........................................................................
+
+    // const userCollection = client.db('forumDB').collection('users');
+    const postsCollection = client.db('forumDB').collection('posts');
+    const commentCollection = client.db('forumDB').collection('comments');
+
+
+    // ........................CRUD start..............................
+    // get all posts
+    app.get('/posts', async (req, res) => {
+      const cursor = postsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    // insert post
+    app.post('/posts', async (req, res) => {
+      const post = req.body;
+      console.log(post);
+      const result = await postsCollection.insertOne(post);
+      res.send(result);
+    })
+
+    // update
+    app.get('/posts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await postsCollection.findOne(query);
+      res.send(result);
+    })
+
+    // update
+    app.put('/posts/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true };
+      const updatedInfo = req.body;
+      const updated = {
+        $set: {
+          desc: updatedInfo.desc
+        }
+      }
+      const result = await postsCollection.updateOne(filter, updated, options);
+      res.send(result);
+    })
+
+    // delete
+    app.delete('/posts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await postsCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    // ........................CRUD end..............................
+
+
+    // comments start
+     // insert comment
+     app.post('/comment', async (req, res) => {
+      const comment = req.body;
+      console.log(comment);
+      const result = await commentCollection.insertOne(comment);
+      res.send(result);
+    })
+
+     // get all comments
+     app.get('/comment', async (req, res) => {
+      const cursor = commentCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    
+
+     // update
+     app.get('/comment/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await commentCollection.findOne(query);
+      res.send(result);
+    })
+
+    // ...........................................................................
+
+
+
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -36,11 +131,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-// ...........................................................................
 app.get('/', (req, res) => {
-    res.send('Forum server is running')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Forum server is running on port ${port}`)
-  })
+  res.send('Forum server is running')
+})
+
+app.listen(port, () => {
+  console.log(`Forum server is running on port ${port}`)
+})
